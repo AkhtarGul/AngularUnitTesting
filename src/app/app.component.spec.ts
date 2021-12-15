c7e025f4f28f152ff32b225dc78c240e81719644
Akhtar <akhtar@gmail.com>
2021-12-15 22:48:54 +0500

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,22 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularUnitTesting'`, () => {
+  it(`should have as title 'Angular Unit Testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularUnitTesting');
+    expect(app.title).toEqual('Angular Unit Testing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularUnitTesting app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Unit Testing');
+  });
+  it('should render name', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('p')?.textContent).toContain('come');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularUnitTesting';
+  title = 'Angular Unit Testing';
+  name='come';
+  testing(){
+    alert("hi I am test");
+  }
 }
